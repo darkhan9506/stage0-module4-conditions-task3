bc6e728c1a92923eb6373e67e090db07f06c608c
Darkhan Zhursin <daratechn@gmail.com>
2024-01-29 21:00:23 -0600

    done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        switch (character) {
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'B':
+                System.out.println("Consonant");
+                break;
+            default:
+                System.out.println("wrong alphabet!");
+                break;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+//        char[] engLayout = {'r', 'R', 'a', 'A'};
+//
+//        for (char c : engLayout) {
+//            if (symbol == c) {
+//                System.out.println("English");
+//            }
+//        }
+
+        if (symbol == 'r' || symbol == 'R') {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
